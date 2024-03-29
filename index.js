eb90d43ec03820e335fbc88e9cb8520934786263
Madiha khan <haideraqeel86@hotmail.com>
2024-03-29 16:33:30 +0500

    design completed

--- index.js
+++ index.js
@@ -164,6 +164,7 @@
     });
 
     // Supply chart
+    const referenceDataValue = 300;
     const chartSupplyData = {
         labels: ['Apr 23', 'May 23 ',' Jun 23', 'July 23 ', 'Aug 23', 'Sep 23' ,'Oct 23', 'Nov 23' ,'Dec 24', 'Jan 23', 'Feb 23'],
         datasets: [{
@@ -210,13 +211,20 @@
             datalabels: {
                 anchor: 'end',
                 align: 'top',
-                color: 'red',
-                // formatter: function(value, context) {
-                //     return value.toLocaleString() + '%';
-                // }
+                color:'red',
                 formatter: function(value, context) {
-                    return "+"+parseInt((value.toLocaleString()/1000)*100) +'%';
-                }
+                    let diff = (value - referenceDataValue) / referenceDataValue * 100;
+                    diff = parseFloat(diff.toFixed(2));
+                    const prefix = diff >= 0 ? "+" : "";
+                    // const color = diff >= 0 ? '#DD5855' :'#3FBE12' ;
+                    // context.dataset.datalabels. = color;
+                    return prefix + diff + '%';
+                    // return prefix + diff + '%'
+                },
+                color: function(context) {
+                    const diff = (context.dataset.data[context.dataIndex] - 300) / 300 * 100;
+                    return diff >= 0 ? '#3FBE12' : '#DD5855';
+                },
             }
         }
     };
@@ -234,21 +242,21 @@
         labels: ['Apr 23', 'May 23 ',' Jun 23', 'July 23 ', 'Aug 23', 'Sep 23' ,'Oct 23', 'Nov 23' ,'Dec 24', 'Jan 23', 'Feb 23'],
         datasets: [{
             label: 'Median Demand Price',
-            data: [100,450, 351, 200, 330, 435,450, 351, 200, 330, 435],
+            data: [1000,4500, 3510, 2000, 3300, 4350,4500, 3510, 2000, 3300, 4350],
             backgroundColor: '#19271F',
             borderColor: '#19271F',
             borderWidth: 1,
             barThickness: 40
         }]
     };
-
+    const referenceValue = 3000;
     const chartDemandOptions = {
         scales: {
             y: {
                 min:0,
-                max:500,
+                max:6000,
                 ticks: {
-                    stepSize: 100,
+                    stepSize: 1000,
                     callback: function(value, index, values) {
                         return value.toLocaleString();
                     },
@@ -276,13 +284,23 @@
             datalabels: {
                 anchor: 'end',
                 align: 'top',
-                color: 'red',
-                // formatter: function(value, context) {
-                //     return value.toLocaleString() + '%';
-                // }
+                color:'red',
                 formatter: function(value, context) {
-                    return "+"+parseInt((value.toLocaleString()/1000)*100) +'%';
-                }
+                    let diff = (value - referenceValue) / referenceValue * 100;
+                    diff = parseFloat(diff.toFixed(2));
+                    const prefix = diff >= 0 ? "+" : "";
+                    // const color = diff >= 0 ? '#3FBE12' : 'red';
+                    // console.log("Color: ", color); 
+                    // context.dataset.datalabels. = color;
+                    return prefix + diff + '%' ;                    
+                    // return prefix + diff + '%'
+                    
+                },
+                color: function(context) {
+                    const diff = (context.dataset.data[context.dataIndex] - 3000) / 3000 * 100;
+                    return diff >= 0 ? '#3FBE12' : '#DD5855';
+                },
+                
             }
         }
     };
@@ -298,55 +316,216 @@
 
     // doughnut chrt
 
+    const scrnWidth = window.innerWidth;
 
-    const data = {
-        // labels: ['January', 'February', 'March', 'April', 'May'],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [200, 100, 200],
-          backgroundColor: [
-            '#C4D56D',
-            '#F3EE60',
-            '#DD5855'
-          ],
-          cutout:'75%',
-          circumference:180,
-          rotation:-90,
-            hoverOffset: 0,
-            
-        }],
-        options: {
-        plugins: {
-            tooltip: {
-                enabled: true
+    Highcharts.chart('doughnutcontainer', {
+
+    chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false,
+        height: '81%',
+        marginLeft: 50,
+        marginRight: 50,
+        marginBottom: scrnWidth > 1050 ? 0 : 50
+    },
+
+    title: {
+        text: ''
+    },
+
+    pane: {
+        startAngle: -90,
+        endAngle: 90,
+        background: null,
+        center: ['50%', '75%'],
+        size: '115%',
+    },
+
+    // the value axis
+    yAxis: {
+        min: 5,
+        max: 7.5,
+        tickInterval: 0.5,
+        tickPixelInterval: scrnWidth > 1330 ? 250 : scrnWidth > 1155 ? 200 : scrnWidth > 945 ? 150 : 100,
+        tickPosition: 'outside',
+        tickColor: Highcharts.defaultOptions.chart.backgroundColor || '#FFFFFF',
+        tickLength: 20,
+        tickWidth: 1,
+        minorTickInterval: null,
+        labels: {
+            distance: scrnWidth > 1330 ? 30 : 25,
+            style: {
+                fontSize: scrnWidth > 1330 ? '25px' : scrnWidth > 1155 ? '20px' : '15px'
+            }
+        },
+        lineWidth: 0,
+        plotBands: [{
+            from: 5.0,
+            to: 6.0,
+            color: '#C4D56D', // green
+            thickness: 65
+        }, {
+            from: 6.0,
+            to: 6.5,
+            color: '#F3EE60', // yellow
+            thickness: 65
+        }, {
+            from: 6.5,
+            to: 7.5,
+            color: '#DD5855', // red
+            thickness: 65
+        }]
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [6.91],
+        tooltip: {
+            valueSuffix: ''
+        },
+        dataLabels: {
+            format: '{y}',
+            borderWidth: 0,
+            color: (
+                Highcharts.defaultOptions.title &&
+                Highcharts.defaultOptions.title.style &&
+                Highcharts.defaultOptions.title.style.color
+            ) || '#333333',
+            style: {
+                fontSize: '43px',
+                fontFamily: 'Sculpin'
             },
-            legend: {
-                display: false
-            },
-            layout: {
-                padding: {
-                  top: 0,
-                  bottom: 30,
-                },
-              },
-            datalabels: {
-                anchor: 'end',
-                align: 'bottom',
-                color: 'black',
-                formatter: function(value, context) {
-                    return value.toLocaleString() + '%';
-                }
-                // formatter: function(value, context) {
-                //     return parseInt((value.toLocaleString()/1000)*100);
-                // }
-            },
-            
+            verticalAlign: 'bottom',
+            y: 120,
+            x: -12
+        },
+        dial: {
+            radius: '80%',
+            backgroundColor: 'black',
+            baseWidth: 12,
+            baseLength: '0%',
+            rearLength: '0%'
+        },
+        pivot: {
+            backgroundColor: 'black',
+            radius: 25
         }
-    }
-      };
-        
-      const ctxDoughnut = document.getElementById('Doughnut').getContext('2d');
-    const doughnutChart = new Chart(ctxDoughnut, {
-        type: 'doughnut',
-        data: data,
-    })
+
+    }]
+
+});
+   
+//    second doughnut
+
+
+
+    const screenWidth = window.innerWidth
+
+    Highcharts.chart('doughnutChart', {
+  
+      chart: {
+          type: 'gauge',
+          plotBackgroundColor: null,
+          plotBackgroundImage: null,
+          plotBorderWidth: 0,
+          plotShadow: false,
+          height: '81%',
+          marginBottom: 100,
+          // width: '60%'
+      },
+  
+      title: {
+          text: ''
+      },
+  
+      pane: {
+          startAngle: -90,
+          endAngle: 90,
+          background: null,
+          center: ['50%', '75%'],
+          size: '115%',
+      },
+  
+      // the value axis
+      yAxis: {
+          min: 830000,
+          max: 850000,
+          tickInterval: 5000,
+          tickPixelInterval: screenWidth > 1330 ? 250 : screenWidth > 1155 ? 200 : screenWidth > 945 ? 150 : 100,
+          tickPosition: 'outside',
+          tickColor: Highcharts.defaultOptions.chart.backgroundColor || '#FFFFFF',
+          tickLength: 20,
+          tickWidth: 1,
+          minorTickInterval: null,
+        //   labels: {
+        //       distance: screenWidth > 1330 ? 30 : 25,
+        //       style: {
+        //           fontSize: screenWidth > 1330 ? '25px' : screenWidth > 1155 ? '20px' : '15px'
+        //       }
+        labels: {
+            distance: 20,
+            rotation: 'auto',
+            formatter: function() {
+                return this.value.toLocaleString();
+            },
+            style: {
+                fontSize: '25px'
+            }
+        // },
+          },
+          lineWidth: 0,
+          plotBands: [{
+            from: 830000,
+            to: 850000,
+            color: {
+                linearGradient: { x1: 0, y1: 0, x2: 1, y2: 0 },
+                stops: [
+                    [0.0398, '#F0D251'], // 3.98%
+                    [0.4953, '#91CF4B'], // 49.53%
+                    [0.9716, '#E97634']  // 97.16%
+                ]
+            },
+            thickness: 65
+        }]
+      },
+  
+      series: [{
+          name: 'Speed',
+          data: [840000],
+          tooltip: {
+              valueSuffix: ''
+          },
+          dataLabels: {
+              format: '{y}',
+              borderWidth: 0,
+              color: (
+                  Highcharts.defaultOptions.title &&
+                  Highcharts.defaultOptions.title.style &&
+                  Highcharts.defaultOptions.title.style.color
+              ) || '#333333',
+              style: {
+                  fontSize: '43px',
+                  fontFamily: 'Sculpin',
+              },
+              verticalAlign: 'bottom',
+              y: 120,
+              x: -12
+          },
+          dial: {
+              radius: '80%',
+              backgroundColor: 'black',
+              baseWidth: 12,
+              baseLength: '0%',
+              rearLength: '0%'
+          },
+          pivot: {
+              backgroundColor: 'black',
+              radius: 25
+          }
+  
+      }]
+  
+  });
